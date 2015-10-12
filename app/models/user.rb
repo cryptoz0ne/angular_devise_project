5a2d81e33aed88c89ee0567e9ffdebda0f3eb3d0
seven3673477 <kay2673477@gmail.com>
2015-10-12 21:57:36 +0200

    Finish Angular project

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,26 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  # def update_with_password(params={})
+  #   current_password = params.delete(:current_password)
+
+  #   if params[:password].blank?
+  #     params.delete(:password)
+  #     params.delete(:password_confirmation) if params[:password_confirmation].blank?
+  #   end 
+  #   valid_pwd = valid_password?(current_password) 
+  #   result = if current_password.blank? || valid_pwd
+  #     update_attributes(params)
+  #   else
+  #     self.errors.add(:current_password, current_password.blank? ? :blank : :invalid)
+  #     self.attributes = params
+  #     false
+  #   end 
+
+  #   clean_up_passwords
+  #   result
+  # end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
-  get 'get-current-user', to: 'visitors#get_current_user'
 end
