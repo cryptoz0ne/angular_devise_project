9b87379084cf77895e94052f5e2cadd3fcbdc773
seven3673477 <kay2673477@gmail.com>
2015-10-14 00:29:28 +0200

    Remove jquery validation and add angular validation

--- app/assets/javascripts/angular/main.js
+++ app/assets/javascripts/angular/main.js
@@ -24,144 +24,169 @@ app.controller("TestCtrl",function(Auth, $scope) {
   };
 
 
-  $scope.submitLogin = function() {
-		angular.element("#loginform").validate({
-			submitHandler: ajaxLogin,
-		  rules: {
-		    // simple rule, converted to {required:true}
-		    // compound rule
-		    log_email: {
-		      required: true,
-		      email: true
-		    },
-		    log_password: {
-		    	required: true,
-		    	minlength: 8
-		    }
-		  },
-		  messages: {
-		    log_email: {
-		      required: "We need your email address to contact you",
-		      email: "Your email address must be in the format of name@domain.com"
-		    },
-		    log_password: {
-		    	required:	"Please input your password",
-		    	minlength: jQuery.validator.format("Enter at least {0} characters")
-		    }
-		  },
-		  errorPlacement: function(error, element) {
-		  	angular.element('#errdiv p').html('');
-		  	angular.element('#errdiv p').addClass('bg-danger');
-				error.appendTo(angular.element('#errdiv p'));
-			}
-		});
-  }
+// jQuery validation
+  // $scope.submitLogin = function() {
+		// angular.element("#loginform").validate({
+		// 	submitHandler: ajaxLogin,
+		//   rules: {
+		//     // simple rule, converted to {required:true}
+		//     // compound rule
+		//     log_email: {
+		//       required: true,
+		//       email: true
+		//     },
+		//     log_password: {
+		//     	required: true,
+		//     	minlength: 8
+		//     }
+		//   },
+		//   messages: {
+		//     log_email: {
+		//       required: "We need your email address to contact you",
+		//       email: "Your email address must be in the format of name@domain.com"
+		//     },
+		//     log_password: {
+		//     	required:	"Please input your password",
+		//     	minlength: jQuery.validator.format("Enter at least {0} characters")
+		//     }
+		//   },
+		//   errorPlacement: function(error, element) {
+		//   	angular.element('#errdiv p').html('');
+		//   	angular.element('#errdiv p').addClass('bg-danger');
+		// 		error.appendTo(angular.element('#errdiv p'));
+		// 	}
+		// });
+  // }
 
 
-  var ajaxLogin = function () {
+  $scope.submitLogin = function () {
 	  var config_login = {
 	      headers: {
 	          'X-HTTP-Method-Override': 'POST'
 	      }
 	  };
 
+	  if (Object.keys($scope.loginform.$error).length != 0)
+    	return false;
+    
 	  Auth.login($scope.credentials_login, config_login).then(function(user) {
+	  	debugger;
 	      console.log(user); // => {id: 1, ect: '...'}
 	      $scope.currentUser = user;
 	      $scope.message = "Logged in successfully!";
-	      var errormessage = "<p class='bg-success' style='font-weight:bold;'>"+"<label>"+$scope.message+"</label></p>"
-	      angular.element('#errdiv p').html(errormessage);
+	      angular.element("#infodiv").removeClass("alert-danger");
+	      angular.element("#infodiv").removeClass("alert-info");
+	      angular.element("#infodiv").addClass("alert-success");
+	      angular.element("#flash_notice").html($scope.message);
+	      
 	  }, function(error) {
 	      // Authentication failed...
 	      $scope.message = error.data.error;
-	      var errormessage = "<p class='bg-danger' style='font-weight:bold;'>"+"<label>"+$scope.message+"</label></p>"
-	      angular.element('#errdiv p').html(errormessage);
-	  });  	
-  }
+	      $scope.message = "Invalid email or password!";
+	      angular.element("#infodiv").removeClass("alert-success");
+	      angular.element("#infodiv").removeClass("alert-info");
+	      angular.element("#infodiv").addClass("alert-danger");
+	      angular.element("#flash_notice").html($scope.message);	      
+	  });
+	}
 
 
   $scope.sign_up = function() {
-   	 angular.element('#errdiv p').html('');
-    	$scope.signup = "1";
+   	angular.element('#errdiv p').html('');
+    $scope.signup = "1";
+    $scope.message = '';
+    angular.element('#titlediv h3').html("Sign up");
   }
 
 
-  $scope.submitSignup = function() {
-		angular.element("#signform").validate({
-			submitHandler: ajaxSignup,
-		  rules: {
-		    // simple rule, converted to {required:true}
-		    // compound rule
-		    first_name: "required",
-		    last_name: "required",
-		    sign_email: {
-		      required: true,
-		      email: true
-		    },
-		    phone_number: "required",
-		    sign_password: {
-		    	required: true,
-		    	minlength: 8
-		    },
-		    sign_password_confirmation: {
-		    	required: true,
-		    	minlength: 8,
-		    	equalTo: "#sign_password"
-		    }
-		  },
-		  messages: {
-		  	first_name: "Enter your firstname",
-		  	last_name: "Enter your lastname",
-		    sign_email: {
-		      required: "We need your email address to contact you",
-		      email: "Your email address must be in the format of name@domain.com"
-		    },
-		    phone_number: "Enter your phonenumber",
-				sign_password: {
-					required: "Provide your password",
-					minlength: jQuery.validator.format("Enter at least {0} characters")
-				},
-				sign_password_confirmation: {
-					required: "Repeat your password",
-					minlength: jQuery.validator.format("Enter at least {0} characters"),
-					equalTo: "Enter the same password as above"
-				}
-		  },
-		  errorPlacement: function(error, element) {
-		  	angular.element('#errdiv p').html('');
-		  	angular.element('#errdiv p').addClass('bg-danger');
-				error.appendTo(angular.element('#errdiv p'));
-			}
-		});
-  }
+  // $scope.submitSignup = function() {
+		// angular.element("#signform").validate({
+		// 	submitHandler: ajaxSignup,
+		//   rules: {
+		//     // simple rule, converted to {required:true}
+		//     // compound rule
+		//     first_name: "required",
+		//     last_name: "required",
+		//     sign_email: {
+		//       required: true,
+		//       email: true
+		//     },
+		//     phone_number: "required",
+		//     sign_password: {
+		//     	required: true,
+		//     	minlength: 8
+		//     },
+		//     sign_password_confirmation: {
+		//     	required: true,
+		//     	minlength: 8,
+		//     	equalTo: "#sign_password"
+		//     }
+		//   },
+		//   messages: {
+		//   	first_name: "Enter your firstname",
+		//   	last_name: "Enter your lastname",
+		//     sign_email: {
+		//       required: "We need your email address to contact you",
+		//       email: "Your email address must be in the format of name@domain.com"
+		//     },
+		//     phone_number: "Enter your phonenumber",
+		// 		sign_password: {
+		// 			required: "Provide your password",
+		// 			minlength: jQuery.validator.format("Enter at least {0} characters")
+		// 		},
+		// 		sign_password_confirmation: {
+		// 			required: "Repeat your password",
+		// 			minlength: jQuery.validator.format("Enter at least {0} characters"),
+		// 			equalTo: "Enter the same password as above"
+		// 		}
+		//   },
+		//   errorPlacement: function(error, element) {
+		//   	angular.element('#errdiv p').html('');
+		//   	angular.element('#errdiv p').addClass('bg-danger');
+		// 		error.appendTo(angular.element('#errdiv p'));
+		// 	}
+		// });
+  // }
 
-  var ajaxSignup = function() {
+ 	$scope.submitSignup = function() {
   	var config_signup = {
         headers: {
             'X-HTTP-Method-Override': 'POST'
         }
     };
 
+	  if (Object.keys($scope.signform.$error).length != 0)
+    	return false;
+    
    	Auth.register($scope.credentials_signup, config_signup).then(function(registeredUser) {
     	  console.log(registeredUser); // => {id: 1, ect: '...'}
     	  
      	 	$scope.signup = "0";
      	 	$scope.message = "You have to confirm your email address before continuing.";
-     	 	var errormessage = "<p class='bg-success' style='font-weight:bold;'>"+"<label>"+$scope.message+"</label></p>"
-	    angular.element('#errdiv p').html(errormessage);
-
+		   	angular.element("#infodiv").addClass("alert-success");
+		    angular.element("#infodiv").removeClass("alert-danger");
+		    angular.element("#infodiv").removeClass("alert-info");
+		    angular.element("#flash_notice").html($scope.message);	  
       
   	}, function(error) {
     	  // Registration failed...
-      	$scope.message = "Email "+error.data.errors.email[0];
-      	var errormessage = "<p class='bg-danger' style='font-weight:bold;'>"+"<label>"+$scope.message+"</label></p>"
-		    angular.element('#errdiv p').html(errormessage);
+    	  console.log(error);
+    	  $scope.message = "Email has already been taken";
+		   	angular.element("#infodiv").removeClass("alert-success");
+		    angular.element("#infodiv").addClass("alert-danger");
+		    angular.element("#infodiv").removeClass("alert-info");
+		    angular.element("#flash_notice").html($scope.message);	 
   	});
-
   }
 
   $scope.submitBack = function() {
   	angular.element('#errdiv p').html('');
+  	$scope.message = "You need to sign in or sign up before continuing.";
+
+    angular.element("#infodiv").removeClass("alert-success");
+    angular.element("#infodiv").removeClass("alert-danger");
+    angular.element("#infodiv").addClass("alert-info");
+    angular.element("#flash_notice").html($scope.message);	  
   	$scope.signup = "0";
   }
 
@@ -175,9 +200,11 @@ app.controller("TestCtrl",function(Auth, $scope) {
     // ...
     Auth.logout(config_logout).then(function(oldUser) {
       $scope.currentUser = false;
-      $scope.message = "Logged-out successfully!";
-      var errormessage = "<p class='bg-danger' style='font-weight:bold;'>"+"<label>"+$scope.message+"</label></p>"
-	    angular.element('#errdiv p').html(errormessage);
+      $scope.message = "Logged out successfully!";
+      angular.element("#infodiv").addClass("alert-success");
+	    angular.element("#infodiv").removeClass("alert-danger");
+	    angular.element("#infodiv").removeClass("alert-info");
+	    angular.element("#flash_notice").html($scope.message);
     }, function(error) {
         // An error occurred logging out.
         $scope.message = error.data.error;
@@ -194,86 +221,99 @@ app.controller("TestCtrl",function(Auth, $scope) {
   }, function(error) {
 
   		$scope.message = "You need to sign in or sign up before continuing.";
-  		var errormessage = "<p class='bg-danger' style='font-weight:bold;'>"+"<label>"+$scope.message+"</label></p>"
-	    angular.element('#errdiv p').html(errormessage);
+
+      angular.element("#infodiv").removeClass("alert-success");
+      angular.element("#infodiv").removeClass("alert-danger");
+      angular.element("#infodiv").addClass("alert-info");
+      angular.element("#flash_notice").html($scope.message);	      
       // unauthenticated error
   });
 });
 
 app.controller("ProfileCtrl", ['$scope','$http', 'Auth', function($scope, $http, Auth) {
-	$scope.chpassword = 0;
-	$scope.errormessage = '';
+	$scope.message = '';
 	$scope.user = {};
 
 	Auth.currentUser().then(function(user){
 		$scope.user = user;
 	});
 
-	$scope.submitUpdate = function() {
-		angular.element("#profile-form").validate({
-			submitHandler: updateProfile,
-		  rules: {
-		    // simple rule, converted to {required:true}
-		    // compound rule
-		    first_name: "required",
-		    last_name: "required",
-		    email: {
-		      required: true,
-		      email: true
-		    },
-		    current_password: "required",
-		    new_password: {
-		    	required: true,
-		    	minlength: 8
-		    },
-		    new_password_confirmation: {
-		    	required: true,
-		    	minlength: 8,
-		    	equalTo: "#new_password"
-		    }
-		  },
-		  messages: {
-		    email: {
-		      required: "We need your email address to contact you",
-		      email: "Your email address must be in the format of name@domain.com"
-		    },
-		    current_password: {
-					required: "Enter your current password",
-					minlength: jQuery.validator.format("Enter at least {0} characters")
-				},
-		    new_password: {
-					required: "Enter new password",
-					minlength: jQuery.validator.format("Enter at least {0} characters")
-				},
-				new_password_confirmation: {
-					required: "Repeat your password",
-					minlength: jQuery.validator.format("Enter at least {0} characters"),
-					equalTo: "Enter the same password as above"
-				}
-		  },
-		  errorPlacement: function(error, element) {
-		  	$scope.errormessage =
-		  	angular.element('#editerr p').html('');
-		  	angular.element('#editerr p').addClass('bg-danger');
-				error.appendTo(angular.element('#editerr p'));
-			}
-		});
-  }
+	//	jQuery validation
+	// $scope.submitUpdate = function() {
+	// 	angular.element("#profile-form").validate({
+	// 		submitHandler: updateProfile,
+	// 	  rules: {
+	// 	    // simple rule, converted to {required:true}
+	// 	    // compound rule
+	// 	    first_name: "required",
+	// 	    last_name: "required",
+	// 	    email: {
+	// 	      required: true,
+	// 	      email: true
+	// 	    },
+	// 	    current_password: "required",
+	// 	    new_password: {
+	// 	    	required: true,
+	// 	    	minlength: 8
+	// 	    },
+	// 	    new_password_confirmation: {
+	// 	    	required: true,
+	// 	    	minlength: 8,
+	// 	    	equalTo: "#new_password"
+	// 	    }
+	// 	  },
+	// 	  messages: {
+	// 	    email: {
+	// 	      required: "We need your email address to contact you",
+	// 	      email: "Your email address must be in the format of name@domain.com"
+	// 	    },
+	// 	    current_password: {
+	// 				required: "Enter your current password",
+	// 				minlength: jQuery.validator.format("Enter at least {0} characters")
+	// 			},
+	// 	    new_password: {
+	// 				required: "Enter new password",
+	// 				minlength: jQuery.validator.format("Enter at least {0} characters")
+	// 			},
+	// 			new_password_confirmation: {
+	// 				required: "Repeat your password",
+	// 				minlength: jQuery.validator.format("Enter at least {0} characters"),
+	// 				equalTo: "Enter the same password as above"
+	// 			}
+	// 	  },
+	// 	  errorPlacement: function(error, element) {
+	// 	  	$scope.errormessage =
+	// 	  	angular.element('#editerr p').html('');
+	// 	  	angular.element('#editerr p').addClass('bg-danger');
+	// 			error.appendTo(angular.element('#editerr p'));
+	// 		}
+	// 	});
+ //  }
 
-	var updateProfile = function() {
+	$scope.submitUpdate = function() {
+
+		if (Object.keys($scope.editform.$error).length != 0)
+    	return false;
+
 		$http({
 			method: 'PATCH',
 			url: '/users.json',
 			data: { user: $scope.user }
 		}).success(function(data){
 			console.log(data);
-			$scope.errormessage = "<p class='bg-success' style='font-weight:bold;'>"+"<label>Edit user successfully!</label></p>"
-			angular.element('#editerr p').html($scope.errormessage);
+			$scope.message = "Edit user successfully!";
+
+      angular.element("#infodiv").addClass("alert-success");
+      angular.element("#infodiv").removeClass("alert-danger");
+      angular.element("#flash_notice").html($scope.message);	 
+			
 		}).error(function(error){
-			debugger;
 			console.log(error);
-			$scope.errormessage = "<p class='bg-success' style='font-weight:bold;'>Email has already been taken or Current_password is invalid.<label>";
-			angular.element('#editerr p').html($scope.errormessage);
+			$scope.message = "Email has already been taken or current password is invalid.";
+
+      angular.element("#infodiv").removeClass("alert-success");
+      angular.element("#infodiv").addClass("alert-danger");
+      angular.element("#flash_notice").html($scope.message);	 
 		});	
 	}
 }]);
